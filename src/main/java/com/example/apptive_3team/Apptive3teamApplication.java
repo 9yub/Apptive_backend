57e39fc59db7ef10b537f964cf6a1c39e2eb45fb
h-hye <euneng356@gmail.com>
2025-05-04 02:22:37 +0900

    Spring Boot project

---
+++ src/main/java/com/example/apptive_3team/Apptive3teamApplication.java
@@ -0,0 +1,13 @@
+package com.example.apptive_3team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apptive3teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apptive3teamApplication.class, args);
+	}
+
+}
