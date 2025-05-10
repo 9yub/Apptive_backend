805f7390138d3f5be7f4e8ee9330a324266f5d7a
h-hye <euneng356@gmail.com>
2025-05-10 23:35:15 +0900

    ping 엔드포인트 추가

--- backend/src/main/java/com/example/apptive_3team/Apptive3teamApplication.java
+++ backend/src/main/java/com/example/apptive_3team/Apptive3teamApplication.java
@@ -2,6 +2,7 @@ package com.example.apptive_3team;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Apptive3teamApplication {
--- backend/src/main/java/com/example/apptive_3team/pingController.java
+++ backend/src/main/java/com/example/apptive_3team/pingController.java
@@ -2,12 +2,16 @@ package com.example.apptive_3team;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class pingPong {
+
+@RestController
+public class pingController {
 
     @GetMapping("/ping")
-    public String pong() {
+    public String ping() {
         return "pong";
     }
+
+
 }
