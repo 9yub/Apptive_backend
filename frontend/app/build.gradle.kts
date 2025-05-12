06077d7af271c158c5ba4c7fc6193d430d2d13f8
Giyun Kim <k_bright_st@pusan.ac.kr>
2025-05-13 00:23:30 +0900

    Feat/4-fe-project_build
    # 변경점 👍
    - 협업을 위해 WOTD FE Android Project를 FE 폴더 내 배치하고, 기본적인 composable을
    작성하였습니다.
    
    # 작성자 ✏
     - 김기윤
    
    Closes #4

---
+++ frontend/app/build.gradle.kts
@@ -0,0 +1,81 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.apptive.wotd"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.apptive.wotd"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val properties = Properties().apply {
+            load(FileInputStream(rootProject.file("local.properties")))
+        }
+        val kakaoKey = properties.getProperty("KAKAO_KEY") ?: ""
+
+        buildConfigField("String", "KAKAO_KEY", "\"$kakaoKey\"")
+        resValue("string", "KAKAO_KEY", kakaoKey)
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    /* FireBase */
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    /* Kakao */
+    implementation("com.kakao.sdk:v2-common:2.20.6")
+    implementation("com.kakao.maps.open:android:2.12.11")
+    implementation("com.kakao.sdk:v2-user:2.20.6")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
