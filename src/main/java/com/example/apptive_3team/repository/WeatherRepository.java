7d69963d1c669dcc7c7e2d4ca4135c3b843564f5
9yub <tbvjzldnl@gmail.com>
2025-05-13 21:03:46 +0900

    KAN-5 <Rename: 패키지 명 변경(domain > entity)>

--- src/main/java/com/example/apptive_3team/repository/WeatherRepository.java
+++ src/main/java/com/example/apptive_3team/repository/WeatherRepository.java
@@ -1,12 +1,11 @@
 package com.example.apptive_3team.repository;
 
-import com.example.apptive_3team.domain.WeatherData;
+import com.example.apptive_3team.entity.WeatherData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
