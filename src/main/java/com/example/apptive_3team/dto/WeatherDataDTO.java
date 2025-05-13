245372b8eef67d81b9281e4532c5c66a6e28eb9c
9yub <tbvjzldnl@gmail.com>
2025-05-13 21:06:55 +0900

    KAN-5 <Chore: 클래스타입을 레코드로 수정>

--- src/main/java/com/example/apptive_3team/dto/WeatherDataDTO.java
+++ src/main/java/com/example/apptive_3team/dto/WeatherDataDTO.java
@@ -1,26 +1,11 @@
 package com.example.apptive_3team.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDate;
 
-@Getter
-@Setter
-public class WeatherDataDTO {
-    private LocalDate date;
-    private double tempFeelsLike;
-    private double tempMin;
-    private double tempMax;
-    private double tempAvg;
-    private double rainAmount;
-
-    public WeatherDataDTO(LocalDate date, double tempFeelsLike, double tempMin, double tempMax, double tempAvg, double rainAmount) {
-        this.date = date;
-        this.tempFeelsLike = tempFeelsLike;
-        this.tempMin = tempMin;
-        this.tempMax = tempMax;
-        this.tempAvg = tempAvg;
-        this.rainAmount = rainAmount;
-    }
+public record WeatherDataDTO(LocalDate date,
+                             double tempFeelsLike,
+                             double tempMin,
+                             double tempMax,
+                             double tempAvg,
+                             double rainAmount) {
 }
--- src/main/java/com/example/apptive_3team/dto/WeatherRequestDTO.java
+++ src/main/java/com/example/apptive_3team/dto/WeatherRequestDTO.java
@@ -1,11 +1,6 @@
 package com.example.apptive_3team.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class WeatherRequestDTO {
-    private double latitude;
-    private double longitude;
+public record WeatherRequestDTO(double latitude,
+                                double longitude){
 }
+
